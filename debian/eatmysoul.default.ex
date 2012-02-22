76b6096397dd24001f41d383cb90665f1f496fba
Elthariel <elthariel@gmail.com>
2012-02-22 13:02:11 +0100

    stop complaining, u dpkg tools

---
+++ debian/eatmysoul.default.ex
@@ -0,0 +1,10 @@
+# Defaults for eatmysoul initscript
+# sourced by /etc/init.d/eatmysoul
+# installed at /etc/default/eatmysoul by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(eatmysoul):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="eatmysoul" command="/usr/bin/eatmysoul"
